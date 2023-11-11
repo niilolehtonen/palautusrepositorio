0665b5399b41ae6813546539077dd85fdd0211ac
niilolehtonen <niilo.i.lehtonen@gmail.com>
2023-11-11 22:30:56 +0200

    ex.10

---
+++ viikko2/nhl-reader/src/index.py
@@ -0,0 +1,25 @@
+import requests
+from player import Player
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
+    response = requests.get(url).json()
+
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        players.append(player)
+
+    print("Players from FIN")
+    print('')
+
+    for player in players:
+        if player.nation == 'FIN':
+            print(player)
+
+if __name__ == "__main__":
+    main()
